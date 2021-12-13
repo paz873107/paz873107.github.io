4552043ae1e97e4f51197f153e0ae00b2d63a2cc
paz873107 <paz873107@gmail.com>
2021-12-12 21:24:06 -0300

    Addition of javascript function that translates the resume to English and back to Spanish

---
+++ main.js
@@ -0,0 +1,9 @@
+function showEnglish(){
+    document.getElementById('English').style.display= 'block';
+    document.getElementById('Español').style.display= 'none';
+}
+
+function mostrarEspañol(){
+    document.getElementById('Español').style.display= 'block';
+    document.getElementById('English').style.display= 'none';
+}
